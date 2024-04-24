ba0fe723bd6a93e85e2b90a10575e632e6d1b68c
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-04-24 21:00:59 +0300

    Advance Java Day 03, Threads

--- src/threads/MultiThreading01.java
+++ src/threads/MultiThreading01.java
@@ -1,4 +1,58 @@
 package threads;
 
 public class MultiThreading01 {
+    public static void main(String[] args) {
+
+        Counter counter1 = new Counter("Sedat");
+        Counter counter2 = new Counter("Tevfik");
+//        counter1.count();
+//        counter2.count();
+
+        System.out.println("=========== multi Threading ===========");
+        Thread thread1 = new ThreadCounter("Mikail");
+        Thread thread2 = new ThreadCounter("Tamara");
+        thread1.start();
+        thread2.start();
+
+    }
+}
+
+class Counter {
+    public String name;
+    public Counter(String name) {
+        this.name = name;
+    }
+    // method to count from 1 to 10
+    public void count() {
+        for (int i=0; i<11; i++) {
+            try {
+                Thread.sleep(500);  // goal is to make it delay for 0.5 seconds
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i + " - " + this.name);
+        }
+    }
+}
+class ThreadCounter extends Thread {
+    public String name;
+    public ThreadCounter(String name) {
+        this.name = name;
+    }
+    // method to count from 1 to 10
+    public void count() {
+        for (int i=0; i<11; i++) {
+            try {
+                Thread.sleep(500);  // goal is to make it delay for 0.5 seconds
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i + " - " + this.name);
+        }
+    }
+
+    @Override
+    public void run() {
+        count();
+    }
 }
