21e0e1f2a59482dcb88305004312b27ae3b2ae27
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-04-27 20:23:07 +0300

    Advance Java Day 05, Multi Threading, ThreadPool, Callable

--- src/threads/DeadLockThreads.java
+++ src/threads/DeadLockThreads.java
@@ -10,10 +10,46 @@ public class DeadLockThreads {
             @Override
             public void run() {
 
-                synchronized ()
+                synchronized (ruler) {
+                    System.out.println(Thread.currentThread().getName() + " is using ruler");
 
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(Thread.currentThread().getName() + " wants to use pencil");
+
+                    synchronized (pencil) {
+                        System.out.println(Thread.currentThread().getName() + " is using pencil");
+                    }
+                }
             }
         });
+        thread1.setName("John");
+        thread1.start();
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                synchronized (pencil) {
+                    System.out.println(Thread.currentThread().getName() + " is using pencil");
+
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(Thread.currentThread().getName() + " wants to use ruler");
+                    synchronized (ruler) {
+                        System.out.println(Thread.currentThread().getName() + " is using ruler");
+                    }
+                }
+            }
+        });
+        thread2.setName("Jerry");
+        thread2.start();
 
     }
 }
