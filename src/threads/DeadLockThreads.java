aceb882fbc5e217d38ed09635974dcb9ecd5244f
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-04-27 20:14:10 +0300

    Advance Java Day 05, Multi Threading, ThreadPool, Callable

---
+++ src/threads/DeadLockThreads.java
@@ -0,0 +1,19 @@
+package threads;
+
+public class DeadLockThreads {
+    public static void main(String[] args) {
+
+        final String ruler = "ruler";
+        final String pencil = "pencil";
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                synchronized ()
+
+            }
+        });
+
+    }
+}
